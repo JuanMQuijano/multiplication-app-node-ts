e0c43938c77f73b48b666607c8fae350607eaa09
Juan Q <jmquijano@unimayor.edu.co>
2023-12-26 23:30:23 -0500

    Deploy

---
+++ src/domain/use-cases/save-file.use-case.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+export interface Options {
+  fileContent: string;
+  fileDestination?: string;
+  fileName?: string;
+}
+
+export interface SaveFileUseCase {
+  execute: (options: Options) => boolean;
+}
+
+export class SaveFile implements SaveFileUseCase {
+  constructor() {}
+
+  execute({
+    fileContent,
+    fileDestination = "outputs",
+    fileName = "table",
+  }: Options): boolean {
+    try {
+      fs.mkdirSync(fileDestination, { recursive: true });
+      fs.writeFileSync(`${fileDestination}/${fileName}.txt`, fileContent);
+      console.log("File Created!");
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
